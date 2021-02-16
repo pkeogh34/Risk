e3c9e42d3def9d82a4df23634ed8511551786377
pkeogh34 <patrick.keogh1@ucdconnect.ie>
2021-02-16 23:42:34 +0000

    Edits

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@
 public class Main {
     public static void main (String args[]) {
         Initialisation initialisation = new Initialisation();
+        GameLogic gameLogic=initialisation.initialisation();
+        gameLogic.diceRoll();
+
     }
 }
